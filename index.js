00e8a0d7fd6cbb2d9c6395ea59063d612a4533f5
Yonatan Kawo <yonas478@gmail.com>
2021-08-11 12:04:29 -0500

    commit tree 66d4108d66b4dd8b7a8cf6d975421ea6f08ff1f9

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+// Moving the dodger to left
+
+const dodger = document.getElementById("dodger"); 
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); 
+
+// Moving the dodger to right
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+  
+    if (right > 0) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  } 
+
+  document.addEventListener("keydown", e => {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  }); 
